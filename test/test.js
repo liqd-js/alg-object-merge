c343ee192514b4ac9cc7e59794fc4269a8d2d888
Tomáš Korenko <radixxko@gmail.com>
2020-05-05 19:00:53 +0200

    Version 1.0.0

---
+++ test/test.js
@@ -0,0 +1,62 @@
+const assert = require('assert' );
+
+const ObjectMerge = require('../lib/merge');
+
+describe( 'Tests', () =>
+{
+    it( 'should merge objects', () =>
+    {
+        assert.deepStrictEqual
+        (
+            ObjectMerge({ foo : 'bar' }, { bar: 'foo' }, { foobar: 'barfoo' }),
+            {
+                foo : 'bar',
+                bar : 'foo',
+                foobar: 'barfoo'
+            },
+            'Didn`t merge objects properly'
+        );
+
+        assert.deepStrictEqual
+        (
+            ObjectMerge({ foo : 'bar', bar: { foo: 'foo' }}, { bar: { foo: 'bar' }}, { foobar: [ 'a', 'b' ]}),
+            {
+                foo : 'bar',
+                bar : { foo : 'bar' },
+                foobar: [ 'a', 'b' ]
+            },
+            'Didn`t merge objects properly'
+        );
+
+        assert.deepStrictEqual
+        (
+            ObjectMerge({ foo : 'bar', bar: { foo: 'foo' }}, { bar: undefined }),
+            {
+                foo : 'bar',
+                bar : { foo : 'foo' }
+            },
+            'Didn`t merge objects properly'
+        );
+
+        assert.deepStrictEqual
+        (
+            ObjectMerge({ foo : [ 1, 2 ], bar: { foo: 'foo' }}, { foo: [ 3, 4 ] }),
+            {
+                foo : [ 3, 4 ],
+                bar : { foo : 'foo' },
+            },
+            'Didn`t merge objects properly'
+        );
+
+
+
+        assert.deepStrictEqual
+        (
+            ObjectMerge({ foo : [ 1, 2 ], bar: { foo: 'foo' }}, { bar: null }),
+            {
+                foo : [ 1, 2 ]
+            },
+            'Didn`t merge objects properly'
+        );
+    });
+});
